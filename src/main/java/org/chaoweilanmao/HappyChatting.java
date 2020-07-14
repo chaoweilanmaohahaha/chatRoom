65da2b701c28c77c0ff1b7fd54ed65517437f139
unknown <1109193348@qq.com>
2020-07-14 22:58:50 +0800

    if website flush or close

--- src/main/java/org/chaoweilanmao/HappyChatting.java
+++ src/main/java/org/chaoweilanmao/HappyChatting.java
@@ -22,14 +22,24 @@ public class HappyChatting {
             System.out.println("Connected!");
             if(webSocket.path().equals("/wudi")) {
                 String url = webSocket.uri();
-                String username = url.substring(url.indexOf('='), url.length());
-                if(userMap.get(username) == null) {
-                    userMap.put(username, webSocket);
-                }
+                String username = url.substring(url.indexOf('=')+1);
+//                if(userMap.get(username) == null) {
+//                    userMap.put(username, webSocket);
+//                }
+
+                // 如果前端刷新了页面，那么也需要刷新这个用户的 websocket
+                userMap.put(username, webSocket);
                 webSocket.handler(Buffer->{
                    String input = Buffer.getString(0, Buffer.length());
                    for(String entry: userMap.keySet()) {
                        ServerWebSocket ws = userMap.get(entry);
+
+                       // 如果前端关闭了连接
+                       if(ws.isClosed()) {
+                           System.out.print(entry + "关闭了连接！");
+                           userMap.remove(entry);
+                           continue;
+                       }
                        ws.writeTextMessage(input);
                    }
                 });
